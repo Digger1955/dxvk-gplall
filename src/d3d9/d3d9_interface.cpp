4586f1c6a609030ea9bc07a919d7cfa7ea9a0262
Laitinlok <adrianlai2000@tutanota.com>
2026-07-06 15:11:05 +0800

    Add back changes from commit acffbbb5584d5d77073e9f9b3d17fc39cbaa68c5

--- src/d3d9/d3d9_interface.cpp
+++ src/d3d9/d3d9_interface.cpp
@@ -525,4 +525,33 @@ namespace dxvk {
     return D3D_OK;
   }
 
+  const D3D9ON12_ARGS* D3D9InterfaceEx::Find9On12Args(
+    const Rc<DxvkAdapter>& Adapter,
+    const D3D9ON12_ARGS*   pOverrides,
+          uint32_t         OverrideCount) {
+    const D3D9ON12_ARGS* arg = nullptr;
+
+#ifdef _WIN32
+    for (uint32_t i = 0u; i < OverrideCount; i++) {
+      if (pOverrides[i].pD3D12Device) {
+        const auto& vk11 = Adapter->devicePropertiesExt().vk11;
+
+        if (vk11.deviceLUIDValid) {
+          Com<ID3D12Device> device = nullptr;
+
+          if (SUCCEEDED(pOverrides[i].pD3D12Device->QueryInterface(__uuidof(ID3D12Device), reinterpret_cast<void**>(&device)))) {
+            LUID luid = device->GetAdapterLuid();
+
+            if (!std::memcmp(&luid, vk11.deviceLUID, sizeof(vk11.deviceLUID)))
+              arg = &pOverrides[i];
+          }
+        }
+      } else if (!arg) {
+        arg = &pOverrides[i];
+      }
+    }
+#endif
+
+    return arg;
+  }
 }
