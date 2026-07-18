8650141918e9990984033c4238abbd870291a8a3
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-07-18 19:15:47 +0200

    [d3d9-dxvk] Port DXVK changes - PR#5773
    [1c11472](https://github.com/doitsujin/dxvk/commit/1c11472cc3f90abe132094597b42587ae0d67d34) - [dxvk] Add helper for unmappable memory blobs - [Rewrite unmaped memory allocator](https://github.com/doitsujin/dxvk/pull/5773)
    
    [31e1787](https://github.com/doitsujin/dxvk/commit/31e1787f0d1075d64bf8604f1dfe2f5f89c98a26) - [d3d9] Use new unmapping code for textures - [Rewrite unmaped memory allocator](https://github.com/doitsujin/dxvk/pull/5773)
    
    [57343d1](https://github.com/doitsujin/dxvk/commit/57343d13811f5875daa602b6313daf292d863636) - [d3d9] Use new unmapping code for shaders - [Rewrite unmaped memory allocator](https://github.com/doitsujin/dxvk/pull/5773)
    
    [8d4115b](https://github.com/doitsujin/dxvk/commit/8d4115b3953f857eb7b31245c778d4947d16e9d8) - [d3d9] Remove old unmapping code - [Rewrite unmaped memory allocator](https://github.com/doitsujin/dxvk/pull/5773)

--- src/d3d9/d3d9_device.h
+++ src/d3d9/d3d9_device.h
@@ -11,7 +11,6 @@
 #include "d3d9_adapter.h"
 #include "d3d9_constant_buffer.h"
 #include "d3d9_constant_set.h"
-#include "d3d9_mem.h"
 
 #include "d3d9_state.h"
 
@@ -39,6 +38,7 @@
 
 #include "../util/util_flush.h"
 #include "../util/util_lru.h"
+#include "../util/util_unmap.h"
 
 namespace dxvk {
 
@@ -1161,7 +1161,7 @@ namespace dxvk {
     /**
      * \brief Returns the allocator used for unmappable system memory texture data
      */
-    D3D9MemoryAllocator* GetAllocator() {
+    MemoryFilePool* GetAllocator() {
       return &m_memoryAllocator;
     }
 
@@ -1584,12 +1584,12 @@ namespace dxvk {
     D3D9Adapter*                    m_adapter;
     Rc<DxvkDevice>                  m_dxvkDevice;
 
-    D3D9MemoryAllocator             m_memoryAllocator;
+    MemoryFilePool                  m_memoryAllocator;
 
     // Second memory allocator used for D3D9 shader bytecode.
     // Most games never access the stored bytecode, so putting that
     // into the same chunks as texture memory would waste address space.
-    D3D9MemoryAllocator             m_shaderAllocator;
+    MemoryFilePool                  m_shaderAllocator;
 
     uint32_t                        m_frameLatency = DefaultFrameLatency;
 
@@ -1703,7 +1703,7 @@ namespace dxvk {
 
     D3D9SwapChainEx*                m_mostRecentlyUsedSwapchain = nullptr;
 
-#ifdef D3D9_ALLOW_UNMAPPING
+#ifdef DXVK_USE_UNMAPPABLE_MEMORY
     lru_list<D3D9CommonTexture*>    m_mappedTextures;
 #endif
 
