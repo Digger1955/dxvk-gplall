07921bf670b6b1182a1dbf114500d185bd362b62
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-08-17 00:19:05 +0200

    Update util_math.h

--- src/util/util_math.h
+++ src/util/util_math.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include <cmath>
+#include <cstddef>
+#include <type_traits>
+
+#if defined(__SSE2__)
+  #include <emmintrin.h>
+#endif
 
 namespace dxvk {
-  
+
   constexpr size_t CACHE_LINE_SIZE = 64;
+
   constexpr double pi = 3.14159265359;
 
   template<typename T>
@@ -13,27 +20,45 @@ namespace dxvk {
     if (n > hi) return hi;
     return n;
   }
-  
+
   template<typename T, typename U = T>
   constexpr T align(T what, U to) {
-    return (what + to - 1) & ~(to - 1);
+    static_assert(std::is_integral_v<T> && std::is_integral_v<U>,
+                  "align(): T and U must be integral types");
+
+    return static_cast<T>(((what + static_cast<T>(to) - 1) / static_cast<T>(to)) * static_cast<T>(to));
   }
+
 
   template<typename T, typename U = T>
   constexpr T alignDown(T what, U to) {
-    return (what / to) * to;
+    static_assert(std::is_integral_v<T> && std::is_integral_v<U>,
+                  "alignDown(): T and U must be integral types");
+    return static_cast<T>((what / static_cast<T>(to)) * static_cast<T>(to));
   }
 
-  // Equivalent of std::clamp for use with floating point numbers
-  // Handles (-){INFINITY,NAN} cases.
-  // Will return min in cases of NAN, etc.
-  inline float fclamp(float value, float min, float max) {
-    return std::fmin(
-      std::fmax(value, min), max);
+  [[nodiscard]] inline float fclamp(float value, float min, float max) {
+  #if defined(__SSE2__)
+
+    __m128 v  = _mm_set_ss(value);
+    __m128 mn = _mm_set_ss(min);
+    __m128 mx = _mm_set_ss(max);
+
+    v = _mm_max_ss(v, mn);
+    v = _mm_min_ss(v, mx);
+
+    float result;
+    _mm_store_ss(&result, v);
+    return result;
+  #else
+
+    return std::fmin(std::fmax(value, min), max);
+  #endif
   }
 
   template<typename T>
   inline T divCeil(T dividend, T divisor) {
+    static_assert(std::is_integral_v<T>, "divCeil(): T must be an integral type");
     return (dividend + divisor - 1) / divisor;
   }
   
