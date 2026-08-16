2ba649c510607f74f9c5b75fe37b80640112794f
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-08-17 00:55:07 +0200

    Update util_vector.h

--- src/util/util_vector.h
+++ src/util/util_vector.h
@@ -1,25 +1,36 @@
 #pragma once
 
 #include <iostream>
+#include <cmath>
+#include <cstddef>
+
+#if defined(__SSE4_1__)
+  #include <emmintrin.h>
+  #include <smmintrin.h>
+#elif defined(__SSE2__)
+  #include <emmintrin.h>
+#endif
 
 #include "util_bit.h"
 #include "util_math.h"
 
 namespace dxvk {
 
+  // ==========================================
+  // GENERAL BASE TEMPLATE (Fallback for Int, etc.)
+  // ==========================================
   template <typename T>
-  struct Vector4Base {
-    Vector4Base()
-      : x{ }, y{ }, z{ }, w{ } { }
+  struct alignas(16) Vector4Base {
+    union {
+      T data[4];
+      struct { T x, y, z, w; };
+      struct { T r, g, b, a; };
+    };
 
-    Vector4Base(T splat)
-      : x(splat), y(splat), z(splat), w(splat) { }
-
-    Vector4Base(T x, T y, T z, T w)
-      : x(x), y(y), z(z), w(w) { }
-
-    Vector4Base(const T xyzw[4])
-      : x(xyzw[0]), y(xyzw[1]), z(xyzw[2]), w(xyzw[3]) { }
+    Vector4Base() : x{ }, y{ }, z{ }, w{ } { }
+    Vector4Base(T splat) : x(splat), y(splat), z(splat), w(splat) { }
+    Vector4Base(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) { }
+    Vector4Base(const T xyzw[4]) : x(xyzw[0]), y(xyzw[1]), z(xyzw[2]), w(xyzw[3]) { }
 
     Vector4Base(const Vector4Base<T>& other) = default;
     Vector4Base& operator=(const Vector4Base<T>& other) = default;
@@ -30,9 +41,8 @@ namespace dxvk {
     bool operator==(const Vector4Base<T>& other) const {
       for (uint32_t i = 0; i < 4; i++) {
         if (data[i] != other.data[i])
-        return false;
+          return false;
       }
-
       return true;
     }
 
@@ -77,7 +87,6 @@ namespace dxvk {
       y += other.y;
       z += other.z;
       w += other.w;
-
       return *this;
     }
 
@@ -86,7 +95,6 @@ namespace dxvk {
       y -= other.y;
       z -= other.z;
       w -= other.w;
-
       return *this;
     }
 
@@ -95,7 +103,6 @@ namespace dxvk {
       y *= scalar;
       z *= scalar;
       w *= scalar;
-
       return *this;
     }
 
@@ -104,43 +111,229 @@ namespace dxvk {
       y /= scalar;
       z /= scalar;
       w /= scalar;
-
       return *this;
     }
-
-    union {
-      T data[4];
-      struct {
-        T x, y, z, w;
-      };
-      struct {
-        T r, g, b, a;
-      };
-    };
-
   };
 
+  // ==========================================
+  // FULLY OPTIMIZED FLOAT SPECIALIZATION
+  // ==========================================
+#if defined(__SSE2__) || defined(__SSE4_1__)
+  template <>
+  struct alignas(16) Vector4Base<float> {
+    union {
+      float data[4];
+      struct { float x, y, z, w; };
+      struct { float r, g, b, a; };
+    };
+
+    Vector4Base() : x{0.0f}, y{0.0f}, z{0.0f}, w{0.0f} { }
+    Vector4Base(float splat) : x(splat), y(splat), z(splat), w(splat) { }
+    Vector4Base(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) { }
+    Vector4Base(const float xyzw[4]) : x(xyzw[0]), y(xyzw[1]), z(xyzw[2]), w(xyzw[3]) { }
+
+    Vector4Base(const Vector4Base<float>& other) = default;
+    Vector4Base& operator=(const Vector4Base<float>& other) = default;
+
+    inline       float& operator[](size_t index)       { return data[index]; }
+    inline const float& operator[](size_t index) const { return data[index]; }
+
+    bool operator==(const Vector4Base<float>& other) const {
+#if defined(__SSE2__)
+      __m128 a = _mm_load_ps(data);         // aligned loads (Vector4Base is alignas(16))
+      __m128 b = _mm_load_ps(other.data);
+      __m128 cmp = _mm_cmpeq_ps(a, b);
+      return _mm_movemask_ps(cmp) == 0xF;
+#else
+      return (x == other.x && y == other.y && z == other.z && w == other.w);
+#endif
+    }
+    bool operator!=(const Vector4Base<float>& other) const { return !operator==(other); }
+
+    Vector4Base operator-() const {
+#if defined(__SSE2__)
+      Vector4Base result;
+      _mm_store_ps(result.data, _mm_sub_ps(_mm_setzero_ps(), _mm_load_ps(data)));
+      return result;
+#else
+      return {-x, -y, -z, -w};
+#endif
+    }
+
+    Vector4Base operator+(const Vector4Base<float>& o) const {
+#if defined(__SSE2__)
+      Vector4Base result;
+      _mm_store_ps(result.data, _mm_add_ps(_mm_load_ps(data), _mm_load_ps(o.data)));
+      return result;
+#else
+      return { x + o.x, y + o.y, z + o.z, w + o.w };
+#endif
+    }
+
+    Vector4Base operator-(const Vector4Base<float>& o) const {
+#if defined(__SSE2__)
+      Vector4Base result;
+      _mm_store_ps(result.data, _mm_sub_ps(_mm_load_ps(data), _mm_load_ps(o.data)));
+      return result;
+#else
+      return { x - o.x, y - o.y, z - o.z, w - o.w };
+#endif
+    }
+
+    Vector4Base operator*(float scalar) const {
+#if defined(__SSE2__)
+      Vector4Base result;
+      __m128 s = _mm_set1_ps(scalar);
+      _mm_store_ps(result.data, _mm_mul_ps(_mm_load_ps(data), s));
+      return result;
+#else
+      return { x * scalar, y * scalar, z * scalar, w * scalar };
+#endif
+    }
+
+    Vector4Base operator*(const Vector4Base<float>& o) const {
+#if defined(__SSE2__)
+      Vector4Base result;
+      _mm_store_ps(result.data, _mm_mul_ps(_mm_load_ps(data), _mm_load_ps(o.data)));
+      return result;
+#else
+      return { x * o.x, y * o.y, z * o.z, w * o.w };
+#endif
+    }
+
+    Vector4Base operator/(const Vector4Base<float>& o) const {
+#if defined(__SSE2__)
+      Vector4Base result;
+      _mm_store_ps(result.data, _mm_div_ps(_mm_load_ps(data), _mm_load_ps(o.data)));
+      return result;
+#else
+      return { x / o.x, y / o.y, z / o.z, w / o.w };
+#endif
+    }
+
+    Vector4Base operator/(float scalar) const {
+      return (*this) * (1.0f / scalar);
+    }
+
+    Vector4Base& operator+=(const Vector4Base<float>& o) {
+#if defined(__SSE2__)
+      _mm_store_ps(data, _mm_add_ps(_mm_load_ps(data), _mm_load_ps(o.data)));
+      return *this;
+#else
+      x += o.x; y += o.y; z += o.z; w += o.w; return *this;
+#endif
+    }
+
+    Vector4Base& operator-=(const Vector4Base<float>& o) {
+#if defined(__SSE2__)
+      _mm_store_ps(data, _mm_sub_ps(_mm_load_ps(data), _mm_load_ps(o.data)));
+      return *this;
+#else
+      x -= o.x; y -= o.y; z -= o.z; w -= o.w; return *this;
+#endif
+    }
+
+    Vector4Base& operator*=(float scalar) {
+#if defined(__SSE2__)
+      __m128 s = _mm_set1_ps(scalar);
+      _mm_store_ps(data, _mm_mul_ps(_mm_load_ps(data), s));
+      return *this;
+#else
+      x *= scalar; y *= scalar; z *= scalar; w *= scalar; return *this;
+#endif
+    }
+
+    Vector4Base& operator/=(float scalar) {
+      return (*this) *= (1.0f / scalar);
+    }
+  };
+#else
+
+  template <>
+  struct alignas(16) Vector4Base<float> {
+    union {
+      float data[4];
+      struct { float x, y, z, w; };
+      struct { float r, g, b, a; };
+    };
+
+    Vector4Base() : x{0.0f}, y{0.0f}, z{0.0f}, w{0.0f} { }
+    Vector4Base(float splat) : x(splat), y(splat), z(splat), w(splat) { }
+    Vector4Base(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) { }
+    Vector4Base(const float xyzw[4]) : x(xyzw[0]), y(xyzw[1]), z(xyzw[2]), w(xyzw[3]) { }
+
+    Vector4Base(const Vector4Base<float>& other) = default;
+    Vector4Base& operator=(const Vector4Base<float>& other) = default;
+
+    inline       float& operator[](size_t index)       { return data[index]; }
+    inline const float& operator[](size_t index) const { return data[index]; }
+
+    bool operator==(const Vector4Base<float>& other) const {
+      return (x == other.x && y == other.y && z == other.z && w == other.w);
+    }
+    bool operator!=(const Vector4Base<float>& other) const { return !operator==(other); }
+
+    Vector4Base operator-() const { return {-x, -y, -z, -w}; }
+    Vector4Base operator+(const Vector4Base<float>& o) const { return {x + o.x, y + o.y, z + o.z, w + o.w}; }
+    Vector4Base operator-(const Vector4Base<float>& o) const { return {x - o.x, y - o.y, z - o.z, w - o.w}; }
+    Vector4Base operator*(float scalar) const { return {x * scalar, y * scalar, z * scalar, w * scalar}; }
+    Vector4Base operator*(const Vector4Base<float>& o) const { return {x * o.x, y * o.y, z * o.z, w * o.w}; }
+    Vector4Base operator/(const Vector4Base<float>& o) const { return {x / o.x, y / o.y, z / o.z, w / o.w}; }
+    Vector4Base operator/(float scalar) const { return (*this) * (1.0f / scalar); }
+
+    Vector4Base& operator+=(const Vector4Base<float>& o) { x += o.x; y += o.y; z += o.z; w += o.w; return *this; }
+    Vector4Base& operator-=(const Vector4Base<float>& o) { x -= o.x; y -= o.y; z -= o.z; w -= o.w; return *this; }
+    Vector4Base& operator*=(float scalar) { x *= scalar; y *= scalar; z *= scalar; w *= scalar; return *this; }
+    Vector4Base& operator/=(float scalar) { return (*this) *= (1.0f / scalar); }
+  };
+#endif
+
+  // ==========================================
+  // UTILITY NON-MEMBER MATH FUNCTIONS
+  // ==========================================
   template <typename T>
   inline Vector4Base<T> operator*(T scalar, const Vector4Base<T>& vector) {
     return vector * scalar;
   }
 
-  template <typename T>
-  float dot(const Vector4Base<T>& a, const Vector4Base<T>& b) {
-    return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+#if defined(__SSE4_1__)
+  inline float dot(const Vector4Base<float>& a, const Vector4Base<float>& b) {
+    __m128 res = _mm_dp_ps(_mm_load_ps(a.data), _mm_load_ps(b.data), 0xF1);
+    float out;
+    _mm_store_ss(&out, res);
+    return out;
   }
+#elif defined(__SSE2__)
+
+  inline float dot(const Vector4Base<float>& a, const Vector4Base<float>& b) {
+    __m128 mul = _mm_mul_ps(_mm_load_ps(a.data), _mm_load_ps(b.data));
+
+    __m128 shuf = _mm_shuffle_ps(mul, mul, _MM_SHUFFLE(1, 0, 3, 2));
+    __m128 sums = _mm_add_ps(mul, shuf);
+    shuf = _mm_shuffle_ps(sums, sums, _MM_SHUFFLE(2, 3, 0, 1));
+    __m128 total = _mm_add_ps(sums, shuf);
+    float out;
+    _mm_store_ss(&out, total); // store lowest float
+    return out;
+  }
+#else
+  template <typename T>
+  inline float dot(const Vector4Base<T>& a, const Vector4Base<T>& b) {
+    return float(a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w);
+  }
+#endif
 
   template <typename T>
-  T lengthSqr(const Vector4Base<T>& a) { return dot(a, a); }
+  inline T lengthSqr(const Vector4Base<T>& a) { return dot(a, a); }
 
   template <typename T>
-  float length(const Vector4Base<T>& a) { return std::sqrt(float(lengthSqr(a))); }
+  inline float length(const Vector4Base<T>& a) { return std::sqrt(float(lengthSqr(a))); }
 
   template <typename T>
-  Vector4Base<T> normalize(const Vector4Base<T>& a) { return a * T(1.0f / length(a)); }
+  inline Vector4Base<T> normalize(const Vector4Base<T>& a) { return a * T(1.0f / length(a)); }
 
   template <typename T>
-  std::ostream& operator<<(std::ostream& os, const Vector4Base<T>& v) {
+  inline std::ostream& operator<<(std::ostream& os, const Vector4Base<T>& v) {
     return os << "Vector4(" << v[0] << ", " << v[1] << ", " << v[2] << ", " << v[3] << ")";
   }
 
@@ -149,20 +342,23 @@ namespace dxvk {
 
   static_assert(sizeof(Vector4)  == sizeof(float) * 4);
   static_assert(sizeof(Vector4i) == sizeof(int)   * 4);
+  static_assert(alignof(Vector4) == 16, "Vector4 must be 16-byte aligned");
 
+#if defined(__SSE2__) || defined(__SSE4_1__)
   inline Vector4 replaceNaN(Vector4 a) {
-    #ifdef DXVK_ARCH_X86
     Vector4 result;
-    __m128 value = _mm_loadu_ps(a.data);
-    __m128 mask  = _mm_cmpeq_ps(value, value);
-           value = _mm_and_ps(value, mask);
-    _mm_storeu_ps(result.data, value);
+    __m128 value = _mm_load_ps(a.data);
+    __m128 mask  = _mm_cmpeq_ps(value, value); // NaN != NaN -> mask zero
+    value = _mm_and_ps(value, mask);           // zero-out NaNs
+    _mm_store_ps(result.data, value);
     return result;
-    #else
+  }
+#else
+  inline Vector4 replaceNaN(Vector4 a) {
     for (int i = 0; i < 4; i++)
       a[i] = std::isnan(a[i]) ? 0.0f : a[i];
     return a;
-    #endif
   }
+#endif
 
 }
