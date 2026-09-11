eb2d75b9af2caf273f8bd51dca889236a12b9400
Digger1955 <205152346+Digger1955@users.noreply.github.com>
2026-09-11 06:50:59 +0200

    [dxvk] Port DXVK Low Latency changes
    [cfc119b](https://github.com/netborg-afps/dxvk-low-latency/commit/cfc119b67d07b9691ea7ee24f4f83808a6a05529) - [dxvk] Drop 64-bit requirement for GPU timestamps

--- src/dxvk/framepacer/dxvk_calibrated_device_timestamps.cpp
+++ src/dxvk/framepacer/dxvk_calibrated_device_timestamps.cpp
@@ -1,12 +1,14 @@
 #include "dxvk_calibrated_device_timestamps.h"
 #include "../dxvk_device.h"
 #include <vector>
+#include <assert.h>
 
 namespace dxvk {
 
   CalibratedDeviceTimestamps::CalibratedDeviceTimestamps( DxvkDevice* device )
   : m_device(device),
-    m_timestampPeriod(device->adapter()->deviceProperties().limits.timestampPeriod),
+    m_timestampPeriod(device->properties().core.properties.limits.timestampPeriod),
+    m_timestampValidBits(device->adapter()->getTimestampValidBits()),
     m_canEnable( m_device->vki()->vkGetPhysicalDeviceCalibrateableTimeDomainsEXT != nullptr &&
                m_device->vkd()->vkGetCalibratedTimestampsEXT != nullptr ) {
 
@@ -69,12 +71,38 @@ namespace dxvk {
   }
 
 
+  int64_t getDiff( uint16_t numBits, uint64_t val1, uint64_t val2 ) {
+
+    assert( numBits < 64 );
+    uint64_t mask = (1ULL << numBits)-1;
+
+    uint64_t v1 = val1 & mask;
+    uint64_t v2 = val2 & mask;
+
+    uint64_t diff = (v1 - v2) & mask;
+
+    uint64_t sign_bit = 1ULL << (numBits-1);
+    int64_t res = static_cast<int64_t> (diff);
+
+    // diff is negative, make every "upper bit" 1
+    if (diff & sign_bit) {
+      res = static_cast<int64_t> (diff | ~mask);
+    }
+
+    return res;
+
+  }
+
+
   CalibratedDeviceTimestamps::time_point CalibratedDeviceTimestamps::getHostTimestamp( uint64_t deviceTimestamp ) const {
 
     if (unlikely(m_calibration.deviceTimestamp == 0))
       return time_point{};
 
     int64_t deltaDeviceTicks = deviceTimestamp - m_calibration.deviceTimestamp;
+    if (m_timestampValidBits != 64)
+      deltaDeviceTicks = getDiff( m_timestampValidBits, deviceTimestamp, m_calibration.deviceTimestamp );
+
     int64_t deltaDeviceNanoseconds = deltaDeviceTicks * m_timestampPeriod;
 
     return m_calibration.hostTimestamp + high_resolution_clock::nanoseconds( deltaDeviceNanoseconds );
